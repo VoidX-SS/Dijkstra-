524d78cc8e7c499eaf693ab8408651d118beaa89
VoidX-SS <143262463+VoidX-SS@users.noreply.github.com>
2026-04-10 19:26:44 +0700

    Update CustomLinkedList.cs

--- DataStructures/CustomLinkedList.cs
+++ DataStructures/CustomLinkedList.cs
@@ -1,100 +1,78 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 
-internal class Program
+namespace DoAnCuoiKy_Dijkstra
 {
-    public class Node
-    { 
-        public object TenThanhPho;
-        public Node blink, flink;
-        public Node()
-        {
-            TenThanhPho = null;
-            flink = blink = null;
-        }
-        public Node(object ThanhPhoMoi)
-        {
-            this.TenThanhPho = ThanhPhoMoi;
-            flink = blink = null;
-        }
-
-        public class DoubleLinkedList
-        {
-            public Node header;
-            public DoubleLinkedList()
-            {header = new Node("Header"); }
-
-            private Node TimThanhPho(object value)
-            {
-                Node current = header.flink;
-                while (current != null)
-                {
-                    if (current.TenThanhPho.Equals(value)) return current;
-                    current = current.flink;
-                }
-                return null;
-            }
-
-            public void ThemThanhPho(object newTenThanhPho, object afterTenThanhPho){
-                Node current = TimThanhPho(afterTenThanhPho);
-                Node newnode = new Node(newTenThanhPho);
-                if (current == null) return;
-                if(current.flink!=null)
-                    current.flink.blink = newnode;
-                newnode.flink = current.flink;
-                current.flink = newnode;
-                newnode.blink = current;
-            }
-
-            public void XoaThanhPho(object value) {
-                Node current = TimThanhPho(value);
-                if (current == null) return;
-                if (current.blink != null) {
-                    current.blink.flink = current.flink;
-                if (current.flink != null)
-                    current.flink.blink = current.blink;
-                }
-            }
-
-            public void SortThanhPho()
-            {
-                for (Node i = header; i.flink != null; i = i.flink)
-                    for (Node j = i.flink; j != null; j = j.flink)
-                    {
-                        
-                    }
-            }
-
-            public void DanhSachThanhPho() {
-                Node current = new Node();
-                current = header.flink;
-                while (!(current == null)) {
-                    Console.WriteLine(current.TenThanhPho);
-                    current = current.flink;
-                }
-            }
-            
-
-            public int DemThanhPho()
-            {
-                int Count = 0;
-                Node current = header.flink;
-                while (current != null) 
-                {
-                    Count++;
-                    current = current.flink;
-                }
-
-                return Count;
-            }
-        }
-    }
-    private static void Main(string[] args)
+    public class CustomLinkedList<T>
     {
-        Console.InputEncoding = System.Text.Encoding.UTF8;
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
-        Console.Clear();
+        public ListNode<T> flink { get; private set; }
+        public ListNode<T> blink { get; private set; }
+        public int Count { get; private set; }
 
-        
+        public MyLinkedList()
+        {
+            flink = null;
+            blink = null;
+            Count = 0;
+        }
+
+        // Hàm thêm thành phố
+        public void AddLast(T data)
+        {
+            ListNode<T> newNode = new ListNode<T>(data);
+
+            if (flink == null)
+            {
+                flink = blink = newNode;
+            }
+            else
+            {
+                blink.Next = newNode;
+                newNode.Prev = blink;
+                blink = newNode;
+            }
+            Count++;
+        }
+
+        // Lấy thành phố tại một vị trí index
+        public T GetAt(int index)
+        {
+            if (index < 0 || index >= Count)
+                throw new IndexOutOfRangeException("Vị trí không hợp lệ.");
+
+            ListNode<T> current = flink;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
+            }
+            return current.Data;
+        }
+
+        // Hàm Remove
+        public void Remove(ListNode<T> nodeToRemove)
+        {
+            if (nodeToRemove == null || flink == null) return;
+
+            // Nếu là flink
+            if (nodeToRemove == flink)
+            {
+                flink = nodeToRemove.Next;
+                if (flink != null) flink.Prev = null;
+                else blink = null; // List có 1 phần tử
+            }
+            // Nếu là blink
+            else if (nodeToRemove == blink)
+            {
+                blink = nodeToRemove.Prev;
+                if (blink != null) blink.Next = null;
+            }
+            // Nếu nằm giữa
+            else
+            {
+                nodeToRemove.Prev.Next = nodeToRemove.Next;
+                nodeToRemove.Next.Prev = nodeToRemove.Prev;
+            }
+
+            Count--;
+        }
     }
 }
